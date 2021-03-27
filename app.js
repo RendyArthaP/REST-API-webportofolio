97c543d41f47b33a5563856ad02f41095630eac1
Rendy Artha <rendyartha312@gmail.com>
2021-03-27 21:09:13 +0700

    setup app

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express()
-const port = process.env.port || 3000
-
+const localPort = PORT || 3000
+const { PORT } = require('./config')
 const routes = require('./routes')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`App running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`App running at http://localhost:${PORT}`);
 })
